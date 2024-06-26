651265ff0ae53f5471a21dd6929a12ad75f956b6
Henrik Klev <klevhenrik@gmail.com>
2024-06-26 16:20:42 +0200

    Day 5 (brute force)

---
+++ src/test/kotlin/5Test.kt
@@ -0,0 +1,54 @@
+import klev.`5`
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class `5Test` {
+    @Test
+    fun firstSolution() {
+        assertEquals("35", `5`.firstSolution(testInput))
+        assertEquals("165788812", `5`.firstSolution())
+    }
+
+    @Test
+    fun secondSolution() {
+        assertEquals("46", `5`.secondSolution(testInput))
+        assertEquals("1928058", `5`.secondSolution())
+    }
+
+    private val testInput =
+        """
+        seeds: 79 14 55 13
+
+        seed-to-soil map:
+        50 98 2
+        52 50 48
+        
+        soil-to-fertilizer map:
+        0 15 37
+        37 52 2
+        39 0 15
+        
+        fertilizer-to-water map:
+        49 53 8
+        0 11 42
+        42 0 7
+        57 7 4
+        
+        water-to-light map:
+        88 18 7
+        18 25 70
+        
+        light-to-temperature map:
+        45 77 23
+        81 45 19
+        68 64 13
+        
+        temperature-to-humidity map:
+        0 69 1
+        1 0 69
+        
+        humidity-to-location map:
+        60 56 37
+        56 93 4
+        """.trimIndent().split("\n")
+}
